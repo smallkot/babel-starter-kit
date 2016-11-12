a3c5379b5bdaa249ea4b68776f1d1582f634172b
Константин <kostya_po@mail.ru>
2016-11-12 18:04:14 +0700

    решение задачи 2а

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
-}
-hello('JS World');
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.get('/', (req, res) => {
+  res.json({
+    hello: 'JS World',
+  });
+});
+
+app.get('/2A', (req, res) => {
+  const sum = (+req.query.a || 0) + (+req.query.b || 0);
+  res.send(sum.toString())
+});
+
+app.listen(3000, () => {
+  console.log('Your app listening on port 3000!');
+});
